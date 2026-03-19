5895bb24fffa5fc4415b0ffe297b27f5aa582a52
dembol23 <wdembny.2005@gmail.com>
2026-03-19 18:46:17 +0100

    completed read_log func

--- L3/src/read_log.py
+++ L3/src/read_log.py
@@ -1,6 +1,12 @@
 import sys
 from datetime import datetime
 
+def safe_int(val, default=0):
+    try:
+        return int(val)
+    except (ValueError, TypeError):
+        return default
+
 def ts_to_datetime(ts):
     return datetime.fromtimestamp(float(ts))
 
@@ -8,19 +14,19 @@ def list_to_tuple(line):
     ts = ts_to_datetime(line[0])
     uid = line[1]
     id_orig_host = line[2]
-    id_orig_p = int(line[3])
+    id_orig_p = safe_int(line[3])
     id_resp_h = line[4]
-    id_resp_p = int(line[5])
+    id_resp_p = safe_int(line[5])
     method = line[7]
     host = line[8]
     uri = line[9]
-    status = line[14]
+    status = safe_int(line[14])
 
     return ts, uid, id_orig_host, id_orig_p, id_resp_h, id_resp_p, method, host, uri, status
 
-def read_log(log):
+def read_log():
     sep = "\t"
-    return [list_to_tuple(line.strip().split(sep)) for line in log if line != ""]
+    return [list_to_tuple(line.strip().split(sep)) for line in sys.stdin if line.strip() != ""]
 
 if __name__ == '__main__':
-    print(read_log(sys.stdin))
+    print(read_log()[1:10])
