ce95a54e1449b4c43e96fae9084bac68096c2934
dembol23 <wdembny.2005@gmail.com>
2026-03-25 10:15:37 +0100

    added comments and modified readme

--- L3/src/analyze/get_extension_stats.py
+++ L3/src/analyze/get_extension_stats.py
@@ -9,5 +9,7 @@ def get_extension_stats(log):
             #   rsplit - works like string.split(), but:
             #   1) splits from the right side
             #   2) conducts as many splits as specified in second argument
+            # "archive.tar.gz".split(".", 1)[1]    'tar.gz'  ← od lewej, za dużo
+            # "archive.tar.gz".rsplit(".", 1)[1]   'gz'      ← od prawej, tylko rozszerzenie
             extensions.append(filename.rsplit(".", 1)[1].lower())
     return dict(Counter(extensions))
