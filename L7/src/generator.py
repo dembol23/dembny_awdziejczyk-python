22b610715141f5eef311f9252356aef4f18b5ab0
Devilian1993 <maksioczko@gmail.com>
2026-04-27 09:40:09 +0200

    add mem_make_generator

--- L7/src/generator.py
+++ L7/src/generator.py
@@ -1,10 +1,15 @@
 from typing import Callable, Generator, Any
+from functools import cache, cached_property
+
 
 def make_generator(f: Callable[[int], Any]) -> Generator[Any, None, None]:
     def generator():
         n = 1
         while True:
-            yield(f(n))
-            n +=1
+            yield f(n)
+            n += 1
 
     return generator()
+
+def mem_make_generator(f: Callable[[int], Any]) -> Generator[Any, None, None]:
+    return make_generator(cache(f))
