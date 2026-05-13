d0051494dfa0eba5114b233f6dcd46ab671d7e12
Devilian1993 <maksioczko@gmail.com>
2026-05-13 17:00:57 +0200

    mem_make_generator fixes and add recurrent version

--- L7/src/generator.py
+++ L7/src/generator.py
@@ -1,3 +1,4 @@
+import sys
 from typing import Callable, Generator, Any
 from functools import cache
 
@@ -12,4 +13,14 @@ def make_generator(f: Callable[[int], Any]) -> Generator[Any, None, None]:
     return generator()
 
 def mem_make_generator(f: Callable[[int], Any]) -> Generator[Any, None, None]:
-    return make_generator(cache(f))
+    if not hasattr(f, '_cache'):
+        f._cache = cache(f)
+
+    return make_generator(f._cache)
+
+def mem_make_generator_rec(f):
+    cached_f = cache(f)
+    module = sys.modules[f.__module__]
+    if hasattr(module, f.__name__):
+        setattr(module, f.__name__, cached_f)
+    return make_generator(cached_f)
